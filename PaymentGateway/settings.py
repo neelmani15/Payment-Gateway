d04757f677e48efdcaddfe0b4654430638221bd1
neelmani15 <neelmani242@gmail.com>
2021-12-10 10:51:18 +0530

    Heroku Deployment

--- PaymentGateway/settings.py
+++ PaymentGateway/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
@@ -127,3 +129,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STRIPE_SECRET_KEY = 'sk_test_51K47e0SJn6TkM3OsFxPOyEUxOhTWGuzfYGCONsVDPVrVpp6fCRTBI9NLISqlQfMbnmdHPGrlqLOenCL9eKDFFahf00DkpYAdYq'
 STRIPE_PUBLISHABLE_KEY = 'pk_test_51K47e0SJn6TkM3OsJzePZ2mNzgnTy9Q7O70nbQGp51mC7stzy0hqoAcGR9CupmYTKV2UcprZRec2obcI0tWuKic700kzXmSGxU'
+
+django_heroku.settings(locals())
